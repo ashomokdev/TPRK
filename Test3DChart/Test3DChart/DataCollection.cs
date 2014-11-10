7df95bac37069c5c275251074a14785ac33d4ff5
unknown <y.belyaeva@CORP10.abbyy.ua>
2014-11-10 13:49:57 +0200

    Before giving name of axis

---
+++ Test3DChart/Test3DChart/DataCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Test3DChart
+{
+    public class DataCollection : Collection<SalesData>
+    {
+        public DataCollection()
+        {
+            Add(new SalesData { Actions = "Dogs", WestStoreQuantity = 5, EastStoreQuantity = 7 });
+            Add(new SalesData { Actions = "Cats", WestStoreQuantity = 5, EastStoreQuantity = 6 });
+            Add(new SalesData { Actions = "Birds", WestStoreQuantity = 3, EastStoreQuantity = 8 });
+            Add(new SalesData { Actions = "Fish", WestStoreQuantity = 6, EastStoreQuantity = 9 });
+            Add(new SalesData { Actions = "Fish2", WestStoreQuantity = 8, EastStoreQuantity = 19 });
+        }
+    }
+}
---
+++ Test3DChart/Test3DChart/SalesData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test3DChart
+{
+    public class SalesData
+    {
+        public string Actions { get; set; }
+        public int WestStoreQuantity { get; set; }
+        public int EastStoreQuantity { get; set; }
+    }
+}
