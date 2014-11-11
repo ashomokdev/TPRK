e02f6195cc7898692906c7ba26ec1d3d2f753bcf
unknown <y.belyaeva@CORP10.abbyy.ua>
2014-11-11 10:50:07 +0200

    Before adding costum series.

---
+++ Test3DChart/Test3DChart/AddingLinesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace Test3DChart
+{
+    static class AddingLinesController
+    {
+        public static int LinesCount = GetLinesCount();
+
+        public static int GetLinesCount()
+        {
+            string consequences = Informator.Consequences;
+            string[] split = consequences.Split(',');
+            List<string> list = new List<string>();
+            foreach (string item in split)
+            {
+               list.Add(item.Replace(" ", string.Empty));
+            }
+            List<string> onlyUniqueValues = new List<string>(list.Distinct());
+            return onlyUniqueValues.Count;       
+        }
+    }
+}
---
+++ Test3DChart/Test3DChart/Informator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Test3DChart
+{
+    static class Informator
+    {
+        private static Window1 startWindow = GetStartWindow();
+        private static string actions = GetActions();
+        private static string consequences = GetConsequences();
+
+        private static Window1 GetStartWindow()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(Window1))
+                {
+                    return window as Window1;
+                }
+                else
+                {
+                    throw new Exception("Instance of Window1 not found.");
+                }
+            }
+            return null;
+        }
+
+        private static string GetActions()
+        {
+            return startWindow.textBoxActions.Text;
+        }
+
+        private static string GetConsequences()
+        {
+            return startWindow.textBoxConsequences.Text;
+        }
+
+        public static string Actions
+        {
+            get { return actions; }
+        }
+
+        public static string Consequences
+        {
+            get { return consequences; }
+        }
+
+    }
+}
