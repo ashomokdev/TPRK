96c13a992c0452e9ad387e07b26de611deb5d8e6
unknown <y.belyaeva@CORP10.abbyy.ua>
2014-11-17 12:48:16 +0200

    adding update button

--- Test3DChart/Test3DChart/Informator.cs
+++ Test3DChart/Test3DChart/Informator.cs
@@ -10,7 +10,9 @@ namespace Test3DChart
     {
         private static Window1 startWindow = GetStartWindow();
         private static string actions = GetActions();
+        private static string parameters = GetParameters();
         private static string consequences = GetConsequences();
+
 
         private static Window1 GetStartWindow()
         {
@@ -38,6 +40,89 @@ namespace Test3DChart
             return startWindow.textBoxConsequences.Text;
         }
 
+        private static string GetParameters()
+        {
+            return startWindow.textBoxParameters.Text;
+        }
+
+        public static List<Point> GetConsequences(string action)
+        {
+            List<Point> result = new List<Point>();
+            List<int> allConsequences = GetListConsequences();
+            List<string> allParameters = GetListParameters();
+            List<string> allActions = GetListActions();
+            List<int> consequences = new List<int>();
+            int index = allActions.FindIndex(a => a.Equals(action));
+
+            for (int i = index; i < allConsequences.Count; i = i + allActions.Count)
+            {
+                consequences.Add(allConsequences[i]);
+            }
+            List<int> onlyUniqueValues = new List<int>(consequences.Distinct());
+
+            foreach (int consequence in onlyUniqueValues)
+            {
+                Point point = new Point(action, consequence);
+                result.Add(point);
+            }
+            return result;
+        }
+
+        private static List<int> GetListConsequences()
+        {
+            string consequences = Consequences;
+            string[] split = consequences.Split(',');
+            List<string> list = new List<string>();
+            foreach (string item in split)
+            {
+                list.Add(item.Replace(" ", string.Empty));
+            }
+            List<int> result = new List<int>();
+            foreach (string item in list)
+            {
+                result.Add(Int32.Parse(item));
+            }
+            try
+            {
+                if ((GetListParameters().Count * GetListActions().Count) > result.Count)
+                {
+                    throw new Exception("Не вистачає значень наслідків C, наслідки були згенеровані автоматично значенням за замовчуванням (0).");
+                }
+            }
+            catch (Exception)
+            {
+                while ((GetListParameters().Count * GetListActions().Count) != result.Count)
+                {
+                    result.Add(0);
+                }
+            }
+            return result;
+        }
+
+        public static List<string> GetListActions()
+        {
+            string actions = Actions;
+            string[] split = actions.Split(',');
+            List<string> result = new List<string>();
+            foreach (string item in split)
+            {
+                result.Add(item.Replace(" ", string.Empty));
+            }
+            return result;
+        }
+
+        public static List<string> GetListParameters()
+        {
+            string parameters = Parameters;
+            string[] split = parameters.Split(',');
+            List<string> result = new List<string>();
+            foreach (string item in split)
+            {
+                result.Add(item.Replace(" ", string.Empty));
+            }
+            return result;
+        }
+
         public static string Actions
         {
             get { return actions; }
@@ -48,5 +133,17 @@ namespace Test3DChart
             get { return consequences; }
         }
 
+        public static string Parameters
+        {
+            get { return parameters; }
+        }
+
+        internal static void Update()
+        {
+            actions = GetActions();
+            parameters = GetParameters();
+            consequences = GetConsequences();
+
+        }
     }
 }
